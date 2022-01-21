9e8dd61396c267010f19841000b03ff8801cc211
Viraj Patel <virajpatel325@gmail.com>
2022-01-21 14:08:58 -0500

    generatePassword function is done

--- Generator.js
+++ Generator.js
@@ -10,17 +10,20 @@ const generateEmail = () => {
 
 }
 
-const generatePassword = () => {
-
+const generatePassword = (includeSpecialCharacters, length = 0) => {
+    return includeSpecialCharacters ? randomValue(length > 0 ? length : Math.floor(Math.random() * 20), '#aA!')
+        : randomValue(length > 0 ? length : Math.floor(Math.random() * 20), '#aA!');
 }
 
 const randomValue = (length, code) => {
-    var mask = '';
-    if (code.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
-    if (code.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (code.indexOf('#') > -1) mask += '0123456789';
-    if (code.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
     var result = '';
-    for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+    if (length > 0) {
+        var collection = '';
+        if (code.contains('a')) collection += 'abcdefghijklmnopqrstuvwxyz';
+        if (code.contains('A')) collection += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (code.contains('#')) collection += '0123456789';
+        if (code.contains('!')) collection += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+        for (let i = length; i > 0; --i) result += collection[Math.floor(Math.random() * collection.length)];
+    } else result = 'null';
     return result;
 }
