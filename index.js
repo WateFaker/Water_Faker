3a947b4c20f8ddcd7b1c1c294c3640c517ccaad1
Viraj Patel <virajpatel325@gmail.com>
2022-01-21 01:44:29 -0500

    First function to generate a random user

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
 
 /**
  * What this library needs to do?
- *
+ * 1) Generate Name, Profile_Image, ID, Password, Email with a Promise
+ * 2) return in JSON or XML
+ * 3) Create and validate JWT with custom fields input
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+/*module.exports = async function initialize() {
+    var results;
+    return results;
+};*/
+
+var exports = module.exports = {};
+exports.GenerateRandomUser = function(a) {
+    return new Promise((resolve, reject) => {
+        if (a == null) {
+            resolve("It is null")
+        } else {
+            reject("It is not null")
+        }
+    });
 };
